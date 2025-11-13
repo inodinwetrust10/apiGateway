7a6865bf94dee98735edfc72f3244d541aba9021
inodinwetrust10 <adi4gbsingh@gmail.com>
2025-11-13 10:29:12 +0530

    part 1 completed basic proxy and yaml config

---
+++ gateway.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct{
+	Port string `yaml:"port"`
+	Services []Service `yaml:"services"`
+}
+
+type Service struct{
+	Name string `yaml:"name"`
+	Route string `yaml:"route_pattern"`
+	Url string `yaml:"url"`
+	proxy *httputil.ReverseProxy
+
+}
+
+
+type Gateway struct{
+	Ser map[string]*Service
+}
+
+func NewGateway(cfg *Config)(*Gateway){
+	gw:=&Gateway{
+		Ser: make(map[string]*Service),
+	}
+
+	for i:=range cfg.Services{
+		currSer := &cfg.Services[i]
+
+		target,err:=url.Parse(currSer.Url)
+		if err!=nil{
+			return nil
+		}
+		currSer.proxy=httputil.NewSingleHostReverseProxy(target)
+
+		gw.Ser[currSer.Route]=currSer
+	}
+
+	return gw
+
+}
+
+func (g *Gateway)findService(r *http.Request)*Service{
+	for route,service:=range g.Ser{
+		if strings.HasPrefix(r.URL.Path,route){
+			return service
+		}
+	}
+	return nil
+}
+
+func (g *Gateway)handleGateway(w http.ResponseWriter, r *http.Request){
+	service:=g.findService(r)
+	if service==nil{
+		log.Printf("No service found for the url %s",r.URL.Path)
+		http.Error(w,"No service found for this url",http.StatusNotFound)
+		return
+	}
+
+	log.Printf("forwarding the request of %s path to the service %s",r.URL.Path,service.Name)
+	service.proxy.ServeHTTP(w,r)
+}
+
+
+func loadConfig()(*Config){
+	var cfg Config
+	data,err:=os.ReadFile("config.yaml")
+	if err!=nil{
+		log.Fatal("error loading the config")
+	}
+	yaml.Unmarshal(data,&cfg)
+	return &cfg
+}
+
+
+func main(){
+	cfg:=loadConfig()
+	log.Printf("port %s",cfg.Port)
+	gw:=NewGateway(cfg)
+	mux:=http.NewServeMux()
+	mux.Handle("/",http.HandlerFunc(gw.handleGateway))
+	server:=&http.Server{
+		Addr: cfg.Port,
+		Handler: mux,
+	}
+	err:=server.ListenAndServe()
+	if err!=nil{
+		log.Fatalf("error starting the server on port %s",cfg.Port)
+	}
+}
